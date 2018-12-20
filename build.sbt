b2d63e6611612a48b80517b56c244d7ca2d47014
anyi <759502117@qq.com>
2018-12-21 01:11:02 +0800

    wordcount

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "spark_demo"
 
 version := "0.1"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.0"
--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,6 +1,16 @@
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("wordcount")
+    val sc = new SparkContext(conf)
 
+    val input = sc.textFile("/home/anyi/text.txt")
+
+    val lines = input.flatMap(line=>line.split(" "))
+    val count = lines.map(word=>(word, 1)).reduceByKey{case (x, y)=>x+y}
+
+    val output = count.saveAsTextFile("/home/anyi/sss")
+  }
 }
